a5f71a548d4040ca9f63f7b0ef9285dfb96e28e8
azekyoo <victor.gaspard.mail@gmail.com>
2024-02-29 00:11:44 +0900

    CSS Loader

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -11,7 +11,11 @@ import { ScoreComponent } from "./app/scripts/score";
 
 // Additional main.js logic goes here
 
+
 // TODO #import-css: use ES side-effect imports to import styles/style.css
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import "./app/styles/style.css";
+
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
